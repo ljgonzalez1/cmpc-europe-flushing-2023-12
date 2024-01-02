219e5d7d789b60c55031ca803c07d88a66b1d74c
Luis González <ljgonzalez@uc.cl>
2024-01-02 13:58:51 -0300

    Redefiniciones de: - Conjuntos - Constantes - Parámetros - Variables auxiliares - Naturaleza de las variables

--- model.py
+++ model.py
@@ -17,10 +17,8 @@ Products = {str(product) for product in set(excel_data.get_products())}
 Batches = {str(batch) for batch in set(excel_data.get_batches())}
 
 # -------------------------------- CONSTANTES --------------------------------
-# Máximo tiempo de espera en bodega en días
-T_max = 120
 # Fecha actual
-T_f = time.time() // (3600 * 24)
+T_f = time.time()
 # Importancia de satisfacer la demanda del cliente.
 W_s = 1073741824
 # Importancia de minimizar el exceso o la falta de producto para un cliente.
@@ -28,7 +26,8 @@ W_diff = 65536
 # Base de la potencia para la prioridad de embarque de lotes.
 B = 1.1
 
-# Un número suficientemente grande, debe ser mayor que cualquier valor de D_cp y DDA_cp
+# Un número suficientemente grande, debe ser mayor que cualquier valor de D_cp
+# y DDA_cp
 M = 134217728
 
 
@@ -50,6 +49,15 @@ X_cp = {
         for product in Products
     }
     for client in Clients
+}
+
+batch_product_match = excel_data.get_batch_product_binary()
+P_lp = {
+    batch: {
+        product: batch_product_match[(batch, product)]
+        for product in Products
+    }
+    for batch in Batches
 }
 
 batch_objects = excel_data.get_batches_from_stocks()
@@ -78,7 +86,7 @@ I_c = {  # TODO: Placeholder
 # Fecha de arribo (epoch convertido a días) del lote "l"
 batch_objects = excel_data.get_batches_from_stocks()
 F_l = {
-    batch: batch_objects[batch].shipping_date_epoch // (3600 * 24)
+    batch: batch_objects[batch].shipping_date_epoch
     for batch in Batches
 }
 
@@ -86,15 +94,9 @@ F_l = {
 
 # Tiempo en días que el lote "l" lleva en espera.
 T_l = {
-    batch: max(0, T_f - F_l[batch])
+    batch: max(0, int((T_f - F_l[batch]) // (24 * 3600)))
     for batch in Batches
 }
-
-# Si el lote "l" incorpora el producto "p" en su cargamento
-Pr_lp = pulp.LpVariable.dicts("Pr", ((batch, product)
-                                     for batch in Batches
-                                     for product in Products),
-                              cat='Binary')
 
 # Cantidad efectivamente despachada al cliente "c" del producto "p".
 D_cp = pulp.LpVariable.dicts("D", ((client, product)
@@ -112,7 +114,7 @@ S_cp = pulp.LpVariable.dicts("S", ((client, product)
 # Prioridad por entregar un lote.
 # Aumenta a medida adquiere antigüedad
 R_l = {
-    batch: B ** T_l[batch]
+    batch: max(B ** T_l[batch], 0)
     for batch in Batches
 }
 
@@ -131,6 +133,12 @@ Diff_Neg = pulp.LpVariable.dicts("Diff_Neg", ((client, product)
                                  lowBound=0,
                                  cat='Continuous')
 
+Diff = pulp.LpVariable.dicts("Diff", ((client, product)
+                                      for client in Clients
+                                      for product in Products),
+                             lowBound=0,
+                             cat='Continuous')
+
 # -------------------------- VARIABLES DE DECISIÓN --------------------------
 # Indica si se envía el lote "l" al cliente "c" este mes
 E_lc = pulp.LpVariable.dicts("E", ((batch, client)
@@ -139,12 +147,6 @@ E_lc = pulp.LpVariable.dicts("E", ((batch, client)
                              cat='Binary')
 
 # ------------------------------ RESTRICCIONES -------------------------------
-# 1. Cada lote trae un y solo un producto:
-for l in Batches:
-    model += (pulp.lpSum(Pr_lp[(l, p)]
-                         for p in Products) == 1,
-              f"Un_producto_por_lote_{l.replace(' ', '_')}")
-
 # 2. Definición cantidad despachada al cliente "c" del producto "p":
 # for c in Clients:
 #     for p in Products:
@@ -220,6 +222,18 @@ for c in Clients:
         model += (X_cp[c][p] * M >= D_cp[(c, p)],
                   f"No_vender_al_que_no_quiere_{c}_{p}")
 
+
+
+# ¿?  --  Estoy cansado, esoero acordarme
+## Pr_{lp} \cdot DDA_{cp} \geq E_{lc}
+
+for c in Clients:
+    for p in Products:
+        for l in Batches:
+            model += (X_cp[c][p] * Pr_lp[l][p] >= E_lc[(l, c)],
+                      f"Pr_lp \cdot DDA_cp \geq E_lc___{c}_{p}_{l}")
+
+
 # -------------------------- FUNCIÓN OBJETIVO --------------------------
 objective = (
         # Primer término: Importancia del cliente por satisfacción de demanda
@@ -260,7 +274,7 @@ if pulp.LpStatus[model.status] == 'Optimal':
     print("Solución óptima encontrada!")
     # Filtrar e imprimir solo las variables que comienzan con "E_" y no son 0
     for v in model.variables():
-        # if v.name.startswith("E_") and v.varValue != 0:  # Filtrar por nombre y valor
-        print(f"{v.name} = {v.varValue}")
+        if v.name.startswith("E_") and v.varValue != 0:  # Filtrar por nombre y valor
+            print(f"{v.name} = {v.varValue}")
 else:
     print("No se encontró una solución óptima. Estado:", pulp.LpStatus[model.status])
